2cc49992628e53e085dce78fb0e546567aa009dd
mariutamarius <mariutamarius97@gmail.com>
2021-10-03 18:03:44 +0300

    added firebase setup

---
+++ src/configs/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDV6kWuoSjEqKHbHlQhMOASMrG5rM3E6j0",
+  authDomain: "proiect-react-marius.firebaseapp.com",
+  projectId: "proiect-react-marius",
+  storageBucket: "proiect-react-marius.appspot.com",
+  messagingSenderId: "877553271079",
+  appId: "1:877553271079:web:e7e72d211a5d107deea902"
+};
+  
+export default firebaseConfig;
